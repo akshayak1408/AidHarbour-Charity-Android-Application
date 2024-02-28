6f0f9801f14948ca3650cc2526807d6230856539
Akshaya <akshayakirtivasan14@gmail.com>
2024-02-28 23:23:02 +0530

    Added nav bar

--- AndroidStudioProjects/AidHarbour/app/src/main/java/com/example/aidharbour/MainActivity.kt
+++ AndroidStudioProjects/AidHarbour/app/src/main/java/com/example/aidharbour/MainActivity.kt
@@ -2,10 +2,57 @@ package com.example.aidharbour
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.util.Log
+import android.widget.ImageView
+import android.widget.PopupMenu
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bottomNav: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadFragment(HomeFragment())
+        bottomNav = findViewById(R.id.bottomNav) as BottomNavigationView
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+
+                R.id.about -> {
+                    loadFragment(AboutFragment())
+                    true
+                }
+
+                R.id.profile -> {
+                    loadFragment(ProfileFragment())
+                    true
+                }
+
+                else -> {
+                    false
+                }
+            }
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.my_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
