8a9bf949cbe2861c48907e8e6ee7f1fc412f4be2
Akshaya <akshayakirtivasan14@gmail.com>
2024-04-15 12:23:15 +0530

    AT_Lab

---
+++ OneDrive - Manipal Academy of Higher Education/IT_Labs/Sixth_Sem/AT_Lab/Lab 8/Lab8_q1/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Lab8_q1"
+include(":app")
+ 
